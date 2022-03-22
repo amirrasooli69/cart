72a8c415d6ac1ff198a8285310f7364bc9d7854b
amirrasooli69 <amirrasooli69@gmail.com>
2022-03-22 04:48:23 +0430

    clear debuging

--- src/components/Store.js
+++ src/components/Store.js
@@ -8,8 +8,6 @@ import { ProductsContext } from '../context/ProductContextProvider';
 const Store = () => {
 
     const products = useContext(ProductsContext);
-    console.log(products)
-
     
     return (
         
