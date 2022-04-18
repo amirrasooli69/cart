a7ffca71696c6cdd8e7532337306f2bead4e1d42
amirrasooli69 <amirrasooli69@gmail.com>
2022-04-18 21:13:47 +0430

    fix style ProductDetail

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -2,6 +2,9 @@ import React , { useContext }from 'react';
 import { Link } from 'react-router-dom';
 //Context
 import { ProductsContext } from '../context/ProductContextProvider';
+
+//Styles
+import styles from './ProductDetails.module.css';
 const ProductDetails = (props) => {
 
     const data = useContext(ProductsContext);
@@ -9,14 +12,14 @@ const ProductDetails = (props) => {
     const product = data[id -1];
     const {image , title , description , category , price} = product;
     return (
-        <div>
-            <img src={image} alt="product" />
-            <div>
+        <div className={styles.container}>
+            <img className={styles.image} src={image} alt="product" />
+            <div className={styles.textContainer}>
                 <h3>{title}</h3>
-                <p>{description}</p>
-                <p><span>Category: </span>{category}</p>
-                <div>
-                    <span>{price} $</span>
+                <p className={styles.description}>{description}</p>
+                <p className={styles.category}><span>Category: </span>{category}</p>
+                <div className={styles.buttonContainer}>
+                    <span className={styles.price}>{price} $</span>
                     <Link to='/products'>Back to Shop</Link>
                 </div>
             </div>
