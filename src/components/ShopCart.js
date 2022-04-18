0cbb3e1d834eb205f8e00b87a4b3389492281184
amirrasooli69 <amirrasooli69@gmail.com>
2022-04-18 21:42:14 +0430

    not fix style ShopCart

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -7,37 +7,40 @@ import { CartContext } from '../context/CartContextProvider';
 //Component
 import Cart from './shared/Cart';
 
+//Style
+import styles from './ShopCart.module.css';
+
 const ShopCart = () => {
 
     const {state, dispatch} = useContext(CartContext);
     return (
-        <div>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.cartContainer}>
                 {state.selectedItems.map(item => <Cart key={item.id} data={item}/>)}
             </div>
             {
-                state.itemsCounter > 0 && <div>
+                state.itemsCounter > 0 && <div className={styles.payments}>
                     <p><span>Total Items</span>{state.itemsCounter}</p>
                     <p><span>Total Payment</span>{state.total}</p>
-                    <div>
-                        <button onClick={()=> dispatch({type:'CHECKOUT'})}>Check Out</button>
-                        <button onClick={()=> dispatch({type:'CLEAR'})}>Clear</button>
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.clear} onClick={()=> dispatch({type:'CHECKOUT'})}>Check Out</button>
+                        <button className={styles.checkout} onClick={()=> dispatch({type:'CLEAR'})}>Clear</button>
 
                     </div>
                 </div>
             }
 
             {
-                state.checkout && <div>
-                    <h3>Check Out Successfully</h3>
-                    <Link to='/products'>Buy More</Link>
+                !state.checkout && state.itemsCounter === 0 && <div className={styles.complete}>
+                    <h3>Want to Buy?</h3>
+                    <Link to='/products'>Go To Shop</Link>
                 </div>
             }
 
             {
-                !state.checkout && state.itemsCounter === 0 && <div>
-                    <h3>Want to Buy?</h3>
-                    <Link to='/products'>Go To Shop</Link>
+                state.checkout && <div className={styles.complete}>
+                    <h3>Check Out Successfully</h3>
+                    <Link to='/products'>Buy More</Link>
                 </div>
             }
         </div>
